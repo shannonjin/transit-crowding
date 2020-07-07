66b9fbdba5c79d016b1fd9757fb7b6107b3db643
Shannon Jin <shannonj112@gmail.com>
2020-07-07 15:58:59 -0400

    Initial Commit

---
+++ transit crowdingTests/transit_crowdingTests.swift
@@ -0,0 +1,34 @@
+//
+//  transit_crowdingTests.swift
+//  transit crowdingTests
+//
+//  Created by Shannon Jin on 7/7/20.
+//  Copyright © 2020 Shannon Jin. All rights reserved.
+//
+
+import XCTest
+@testable import transit_crowding
+
+class transit_crowdingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
